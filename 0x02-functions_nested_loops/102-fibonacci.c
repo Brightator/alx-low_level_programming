2a2a179e71c3f35177803f0df2a7e08173c01655
Bright <iambrightoc@gmail.com>
2022-07-06 21:09:22 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -18,7 +18,8 @@ int main(void)
 		next = a + b;
 		a = b;
 		b = next;
-		printf("%lu\n", next);															if (i < 49)
+		printf("\n");
+		printf("%lu", next);															if (i < 49)
 		{
 			printf(", ");
 		}
