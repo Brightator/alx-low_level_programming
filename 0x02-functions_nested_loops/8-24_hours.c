d080a7f33a7c540bb2e5a23131abf1f02a75c5ac
Bright <iambrightoc@gmail.com>
2022-07-06 18:15:51 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,26 +1,31 @@
-#include "main.h"
-/**
- * jack_bauer - prints every minute of the day
+/*
+ * File: 8-24_hours.c
+ *
+ * Auth: Dr Imgawe Marcus
  */
 
+
+#include "main.h"
+
+/**
+ * jack_bauer - Prints every minute of the day of
+ *
+ * Jack Bauer, starting from 00:00 to 23:59.
+ *
+ */
 void jack_bauer(void)
 {
-	int hr, min;
-
-	hr = 0;
-	while (hr < 24)
+	int hour, minute;
+	for (hour = 0; hour <= 23; hour++)
 	{
-		min = 0;
-		while (min < 60)
+		for (minute = 0; minute <= 59; minute++)
 		{
-			_putchar((hr / 10) + '0');
-			_putchar((hr % 10) + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar((min / 10) + '0');
-			_putchar((min / 10) + '0');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
 			_putchar('\n');
-			min++;
 		}
-	hr++;
 	}
 }
