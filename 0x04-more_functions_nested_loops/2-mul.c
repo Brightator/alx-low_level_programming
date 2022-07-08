b3793797e448f55b01ac9ddced770c83f46c8a64
Bright <iambrightoc@gmail.com>
2022-07-08 08:31:01 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers
+ *
+ * @a: character a
+ * @b: character b
+ *
+ * Return: product
+ */
+
+int mul(int a, int b);
+{
+	_putchar("%d", a * b);
+}
