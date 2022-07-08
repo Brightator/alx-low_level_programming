58aa0c3719350a36d8d90df9008a7eb5bdebb464
Bright <iambrightoc@gmail.com>
2022-07-08 09:18:37 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,14 +1,16 @@
 #include "main.h"
+
 /**
  * print_numbers - Outputs 1-9
  *
  * Return: void
  */
+
 void print_numbers(void)
 {
 	char c;
 
-	for (c = '0'; c < '10'; c++)
+	for (c = '0'; c <= '9'; c++)
 	{
 		_putchar(c);
 	}
