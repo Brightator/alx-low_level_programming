eaf8c59767244a96425b8c8ebd1568912b781c2f
Bright <iambrightoc@gmail.com>
2022-07-08 09:05:16 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - Outputs 1-9
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int c;
+
+	c = 0;
+	for (c = 0; c < 10; c++)
+	{
+		_putchar(c);
+	}
+	_putchar("\n")
+}
