61cf25f2298fbdad586e95fd0dc567a48ea1449d
Bright Ogo <106819653+Brightator@users.noreply.github.com>
2022-07-13 02:54:22 +0100

    Create 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a reversed string
+ * @s: string
+ *
+ * Return: reversed string
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != 0)
+	{
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
