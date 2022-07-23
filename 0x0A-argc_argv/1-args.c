a8fb046bed26f76daadd3743fa45791cce08c02e
Bright Ogo <106819653+Brightator@users.noreply.github.com>
2022-07-23 04:14:50 +0100

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - the main entry point of the function
+ * @argc: the argument counter
+ * @argv: array of pointer to strings
+ *
+ * Return: 0
+ */
+
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
