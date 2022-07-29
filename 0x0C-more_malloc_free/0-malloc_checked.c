83329470f81df869a4fd237085bb0b850ad162fd
Bright Ogo <106819653+Brightator@users.noreply.github.com>
2022-07-29 05:20:38 +0100

    0-malloc_checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * malloc_checked - this fxn will cause normal process termination with a status value of 98
+ * malloc_checked - fxn cause normal process termination with a status value of 98
  *
  * @b: allocated memory
  *
  * Return: a pointer to the allocated memory
  */
+
 void *malloc_checked(unsigned int b)
 {
 	void *i;
@@ -15,5 +16,4 @@ void *malloc_checked(unsigned int b)
 	if (i == NULL)
 		exit(98);
 	return (i);
-	
 }
